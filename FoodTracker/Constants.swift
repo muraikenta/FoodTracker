69269e2940de0580ccaf13a606c5bf7d7db64d81
村井謙太 <muraikenta0507@gmail.com>
2016-08-01 22:18:34 +0900

    add sample constants

---
+++ FoodTracker/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  FoodTracker
+//
+//  Created by 村井謙太 on 2016/08/01.
+//  Copyright © 2016年 村井謙太. All rights reserved.
+//
+import UIKit
+
+struct Config {
+    static let baseURL = NSURL(string: "http://www.example.org/")!
+}
+
+struct Color {
+    static let primaryColor = UIColor(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+    static let secondaryColor = UIColor.lightGrayColor()
+}
