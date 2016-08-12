f5d16b0657c8d89e536524cb6acd87e76ce7a051
村井謙太 <muraikenta0507@gmail.com>
2016-08-13 03:33:28 +0900

    create profileViewController

---
+++ FoodTracker/Helpers/ImageHelper.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  FoodTracker
+//
+//  Created by 村井謙太 on 2016/08/13.
+//  Copyright © 2016年 村井謙太. All rights reserved.
+//
+
+import Foundation
+
+class ImageHelper {
+    
+}
